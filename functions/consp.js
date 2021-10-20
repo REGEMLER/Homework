69dbe6e1bf5f85b31a0241f22d5dae6858920015
Yar <yar@gmail.com>
2021-10-20 21:21:34 +0300

    new lection

---
+++ functions/consp.js
@@ -0,0 +1,16 @@
+function russian(name){
+    console.log(`Привет ${name}`)
+}
+
+function english(name){
+    console.log(`Hello ${name}`)
+}
+
+function italian(name){
+    console.log(`ciao ${name}`)
+}
+
+function sayHello(name,translate){
+    
+}
+
