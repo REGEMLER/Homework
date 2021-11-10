16f71e7031c4e8954b7d3c5f018b8b0183cce1b0
Yar <yar@gmail.com>
2021-11-10 21:25:54 +0300

    added clases

---
+++ class/consp.js
@@ -0,0 +1,75 @@
+// 'use strict';
+// function Animal (name, age) {
+//     // 1. this = {};    
+    
+//     this.age = age;
+//     this.name = name;
+//     this.toString = function () {
+//         return 'smth';
+//     };
+//     // 2. return this;
+// }
+
+// Animal.prototype.run = function (where = 'itstep') {
+//     console.log(`${this.name} runs to ${where}`);
+// };
+
+// Animal.prototype.toString = function () {
+//     return this.name;
+// }
+
+// // Animal.prototype === obj.__proto__ === obj2.__proto__ === obj3.__proto__
+
+// let obj = new Animal('Sharik', 10);
+// let obj2 = new Animal('Bobik', 20);
+// let obj3 = new Animal.prototype.constructor('Sobaka', 30);
+
+// console.log(obj3.paws);
+// 1. Написать конструктор товара который принимает 3 параметра
+// название 
+// цвет
+// цена
+// Добавить в прототип классу метод showPrice ===>>> "товар ИМЯ стоит ЦЕНА"
+
+function Product (name, color, price){
+    this.name = name;
+    this.color = color;
+    this.price = price;
+}
+Product.prototype.showPrice = function(){
+    console.log(`Товар ${this.name} стоит ${this.price}`);
+}
+
+let product1 = new Product(`Apple`, `Green`, 18);
+let product2 = new Product(`Repa`, `Red`, 180);
+product2.showPrice();
+
+// 2. Создать класс Device у инстансов которого есть поле isON (boolean)
+// В прототипе класса Device создайте метод .switch() который переключает
+// поле инстанса в true/false при каждом вызове
+
+function Device (){
+    this.isON = true;
+}
+Device.prototype.switch = function(){
+    this.isON = !this.isON;
+}
+
+let Dev1 = new Device();
+console.log(Dev1);
+Dev1.switch();
+console.log(Dev1);
+Dev1.switch();
+console.log(Dev1);
+Dev1.switch();
+console.log(Dev1);
+
+// 3. напишите программу которая вычисляет длину линий в стандартной
+//  двумерной системе координат. Координаты начала и конца линий 
+//  должны хранится в объектах. 
+//  а) Создайте три линии
+//  б) Вычислите их длины
+//  в) Проверьте могут ли три линии сформировать треугольник
+//  Длину линий можно найти по теореме Пифагора
+//  Три линии могут сформировать треугольник если длина каждой стороны 
+//  меньше суммы дву других сторон.
