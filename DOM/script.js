e463c45a83a572a868c79db7ad4d471d53e69da9
Yar <yar@gmail.com>
2021-12-12 11:45:44 +0300

    dom2

--- DOM/script.js
+++ DOM/script.js
@@ -4,6 +4,50 @@
 // Object
 // Model
 
-if (Math.random() > 0.5) {
-    document.bgColor = "orange";
-}
+// if (Math.random() > 0.5) {
+//     document.bgColor = "orange";
+// }
+
+// const lis = document.getElementsByTagName('li');
+
+// console.dir(lis[2]); 
+
+const mainLink = document.body.children[3].children[1].children[1].children[1].children[0].children[2].children[0];
+const second = mainLink.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[1].children[0];
+const h2 = second.parentElement.parentElement.parentElement.previousElementSibling.children[0].children[1];
+console.log(h2.parentElement.parentElement.parentElement.children[1].children[1].children[1].children[0].children[2].children[0]);
+
+const lis = document.getElementsByTagName('li');
+const items = document.getElementsByClassName('item');
+const menu = document.getElementById('menu');
+const shopItems = document.querySelectorAll('#shop > li');
+
+console.dir(menu.getElementsByTagName('li'));
+
+
+
+// changing creating adding removing cloning nodes
+// events
+
+const shop = document.getElementById('shop');
+const items = [...shop.getElementsByTagName('li')];
+
+
+items.forEach((item, index)=>{
+    item.addEventListener('click', () => {
+        console.log(items[index].textContent);
+    });
+})
+
+
+const valerchikFunc = function () {
+    console.log(this);
+};
+
+const shop = document.getElementById('shop');
+const items = [...shop.getElementsByTagName('li')];
+
+
+items.forEach((item, index)=>{
+    item.addEventListener('click', valerchikFunc);
+})
