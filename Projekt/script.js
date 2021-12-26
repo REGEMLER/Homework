a67cdfc05f7cac262eb0d6c3de1dbb3d1a5df3d7
Yar <yar@gmail.com>
2021-12-26 13:12:22 +0300

    treasure

---
+++ Projekt/script.js
@@ -0,0 +1,9 @@
+const map = document.getElementById(`map`);
+const hint = document.getElementById(`hint`);
+
+const treasure = {
+    x:Math.floor(Math.random() * (map.clientWidth - 100)) + 50,
+    y:Math.floor(Math.random() * (map.clientHeight - 100)) + 50
+}
+
+console.log(treasure);
