ba66858328449e31fcbd13a6445bef413df40691
Yar <yar@gmail.com>
2022-01-16 11:18:28 +0300

    lesson2

--- DOM1/index.js
+++ DOM1/index.js
@@ -1,51 +1,98 @@
+// const userInput = document.querySelector('.user-input');
+// const addBtn = document.querySelector('.add');
+// const todoList = document.querySelector('.todo-list');
+
+// const addDiv = () => {
+//     const myDiv = document.createElement('div');
+//     myDiv.classList.add('todo');
+//     myDiv.textContent = userInput.value;
+//     userInput.value = '';
+//     userInput.focus();
+//     todoList.append(myDiv);
+// }
+
+// addBtn.addEventListener('click', addDiv);
+
+// userInput.addEventListener('keydown', ({keyCode}) => {
+//     if (keyCode === 13) {
+//         addDiv();
+//     }
+// });
+// // Добавляет только в  userInput
+
+
+// const addDiv = () => {
+//     if (userInput.value === '') {
+//         return;
+//     }
+//     const myDiv = document.createElement('div');
+//     myDiv.classList.add('todo');
+//     myDiv.textContent = userInput.value;
+//     userInput.value = '';
+//     userInput.focus();
+//     todoList.append(myDiv);
+// };
+
+// addBtn.addEventListener('click', addDiv);
+
+// userInput.addEventListener('keydown', ({keyCode}) => {
+//     if (keyCode === 13) {
+//         addDiv();
+//     }
+// });
+// // Добавляет как надо 
+
+
+// todoList.addEventListener('click', ({target}) => {
+//     if (target.classList.contains('todo')) {
+//         target.remove();
+//     }
+// });
+// // удалили при клике
+/////////////////////////////////////////////////////////////////////////////////////
+
 const userInput = document.querySelector('.user-input');
 const addBtn = document.querySelector('.add');
 const todoList = document.querySelector('.todo-list');
 
-const addDiv = () => {
+
+const generateTodo = (innerText) => {
     const myDiv = document.createElement('div');
     myDiv.classList.add('todo');
-    myDiv.textContent = userInput.value;
-    userInput.value = '';
-    userInput.focus();
-    todoList.append(myDiv);
-}
+    myDiv.insertAdjacentHTML(
+        `afterbegin`,
+        `
+            <i class="fas fa-trash"></i>
+            <i class="far fa-check-circle"></i>
+            <p class="todo__text">${innerText}</p>
+        `);
 
-addBtn.addEventListener('click', addDiv);
+    return myDiv;
+};
 
-userInput.addEventListener('keydown', ({keyCode}) => {
-    if (keyCode === 13) {
-        addDiv();
-    }
-});
-// Добавляет только в  userInput
-
-
-const addDiv = () => {
+const addTodo = () => {
     if (userInput.value === '') {
         return;
     }
-    const myDiv = document.createElement('div');
-    myDiv.classList.add('todo');
-    myDiv.textContent = userInput.value;
+
+    const todo = generateTodo(userInput.value);
     userInput.value = '';
     userInput.focus();
-    todoList.append(myDiv);
+    todoList.append(todo); 
 };
 
-addBtn.addEventListener('click', addDiv);
+addBtn.addEventListener('click', addTodo);
 
 userInput.addEventListener('keydown', ({keyCode}) => {
     if (keyCode === 13) {
-        addDiv();
+        addTodo();
     }
 });
-// Добавляет как надо 
 
 
-todoList.addEventListener('click', ({target}) => {
-    if (target.classList.contains('todo')) {
-        target.remove();
+todoList.addEventListener('click', function ({target}) {
+    if (target.classList.contains('fa-trash')) {
+        // target.parentElement.remove();
+        target.closest('.todo').remove();
     }
 });
-// удалили при клике
